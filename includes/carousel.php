cbeac512e29728ce62b74535f2ba0801f394a574
Vaibhav-Gupta-32 <vaibhavgupta0320@gmail.com>
2024-10-02 12:01:29 +0530

    navbar set

--- includes/carousel.php
+++ includes/carousel.php
@@ -1,6 +1,16 @@
+
+<style>
+    .video-button {
+        width: 100%;
+        position: absolute;
+        top: 40%; /* Adjust this value to move it higher or lower */
+        left: 50%;
+        transform: translate(-50%, -50%);
+    }
+</style>
 <!-- Carousel Start -->
 <div class="container-fluid p-0 pb-5 mt-0 pt-0 wow fadeIn position-relative" data-wow-delay="0.1s">
-    <div class="owl-carousel header-carousel position-relative mt-0">
+    <div class="position-relative mt-0">
 
         <!-- <div class="owl-carousel-item position-relative" data-dot="<img src='img/carousel-1.jpg'>">
             <img class="img-fluid" src="img/carousel-1.jpg" alt="">
@@ -57,37 +67,33 @@
             </div>
         </div> -->
 
-        <div class="owl-carousel-item position-relative" data-dot="<video src='https://cdn.pixabay.com/video/2021/02/07/64381-510398914_large.mp4' muted loop autoplay></video>">
-            <video class="img-fluid" src="https://cdn.pixabay.com/video/2021/02/07/64381-510398914_large.mp4" muted loop autoplay></video>
-            <div class="owl-carousel-inner">
-                <div class="container mt-0">
-                    <div class="row justify-content-center mt-0">
-                        <div class="col-10 col-lg-8">
-                            <h1 class="display-1 text-white text-center fw-bold  animated slideInDown">Best <span class="text-warning">Architecture</span> And Interior Design Services</h1>
-                            <!-- <p class="fs-5 fw-medium text-white mb-4 pb-3">Vero elitr justo clita lorem. Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum et diam justo clita et kasd rebum sea elitr.</p> -->
+        <div class="position-relative" data-dot="<video src='https://cdn.pixabay.com/video/2021/02/07/64381-510398914_large.mp4' muted loop autoplay></video>">
+    <video class="img-fluid" src="https://cdn.pixabay.com/video/2021/02/07/64381-510398914_large.mp4" muted loop autoplay></video>
+    <div class="video-button">
+        <div class="container mt-0">
+            <div class="row  mt-0" style="display: flex;align-items: center;justify-content: space-between;flex-direction: column;">
+                <div class="col-10 col-lg-8">
+                    <h1 class="display-1 text-white text-center fw-bold animated slideInDown">
+                        Best <span class="text-warning">Architecture</span> And Interior Design Services
+                    </h1>
+                </div>
+                <div class="container d-none d-lg-block" style="position: relative;top: 244px;">
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <a href="our-projects.php" class="btn btn-primary text-uppercase py-3 px-5 animated slideInLeft">View Projects</a>
                         </div>
-                        <!-- Container for two buttons -->
-                        <div class="container d-none d-lg-block">
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <!-- Left button -->
-                                    <a href="our-projects.php" class="btn btn-primary text-uppercase py-3 px-5 animated slideInLeft">
-                                    View Projects</a>
-                                </div>
-                                <div class="col-lg-6 d-flex justify-content-end">
-                                    <!-- Right button -->
-                                    <a href="contact-us.php" class="btn btn-secondary text-uppercase fw-bold py-3 px-5 animated slideInRight">Contact-Us</a>
-                                </div>
-                            </div>
+                        <div class="col-lg-6 d-flex justify-content-end">
+                            <a href="contact-us.php" class="btn btn-secondary text-uppercase fw-bold py-3 px-5 animated slideInRight">Contact Us</a>
                         </div>
-                        <!-- Container for two buttons -->
                     </div>
                 </div>
             </div>
         </div>
+    </div>
+</div>
 
 
     </div>
-    <?php include('includes/sidebar.php'); ?>
 </div>
+<?php include('includes/sidebar.php'); ?>
 <!-- Carousel End -->
